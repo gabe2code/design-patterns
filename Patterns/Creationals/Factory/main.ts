34359357aa7294220a51308a1502f570d2b12a8b
gaperezcarballo@gmail.com <gabriel.perez@ntsprint.com>
2020-10-12 18:21:35 -0400

    Adding description to the main to get more context of what's the example about

--- Patterns/Creationals/Factory/main.ts
+++ Patterns/Creationals/Factory/main.ts
@@ -1,3 +1,8 @@
+/**
+ * Imagine a game where you have to avoid obstacles with a ship, some kind of galaxy survival (xD), but you need to choose
+ *  what kind of obstacles you will need to show based on how far you go in the game (this will be more complex based maybe on the level of 
+ * difficulty that you pick, but i just tried to make it simple for the example ). 
+ */
 import ObstaclesFactory from './ObstaclesFactory.ts';
 
 const factory = new ObstaclesFactory();
